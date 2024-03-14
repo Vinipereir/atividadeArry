3e473aaedba438f9293d9b2b88a62bffe342e14f
vinipereira <vinicius.perira.1tds2.senai@gmail.com>
2024-03-14 11:13:32 -0300

    commit01

---
+++ atividade1.js
@@ -0,0 +1,11 @@
+//invertendo a ordem das escitas de tras para frente
+const array = ["a", "b", "c", "d", "e"]
+console.log(array) // ['a','b','c','d','e' ]
+
+//Inverte o array.
+
+array.reverse()
+console.log(array); // ['e','d','c','b','a' ]
+
+
+
