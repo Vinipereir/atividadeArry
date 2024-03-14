2fa7d70d8406f690d5488cfe92587deaa9e0a8d3
vinipereira <vinicius.perira.1tds2.senai@gmail.com>
2024-03-14 11:20:01 -0300

    01

---
+++ 03.js
@@ -0,0 +1,6 @@
+//Juntar todos elementos de array 
+const array = ["a", "b", "c"]
+const string = array.join("-")
+//separar os elementos do array.
+console.log(array) // [ 'a', 'b', 'c' ]
+console.log(string) // a-b-c 
