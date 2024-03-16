0a43d34492208b1c53491125fb28c492fe7fb167
viniciuspereira <viniciuspereira.1tds2.senai@gmail.com>
2024-03-16 16:40:48 -0300

    includes

---
+++ atv10.js
@@ -0,0 +1,14 @@
+//Comando includes
+// Determina se um array contém um determinado elemento
+
+//retornando true ou false conforme apropriado
+
+const array = [1, 2, 3];
+console.log(array.includes(2)); // Saída: true
+console.log(array.includes(4)); // Saída: false
+
+//utilizando um array de frutas
+
+const fruits = ['Maçã', 'Banana', 'Laranja'];
+console.log(fruits.includes('Banana')); // Saída: true
+console.log(fruits.includes('Morango')); // Saída: false
